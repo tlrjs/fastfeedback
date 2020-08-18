8084e2bb8dc4a57a7e5fbbb4ef21498292d4ff60
Tyler Shipe <tjshipe@gmail.com>
2020-08-18 16:59:01 -0400

    Stream 2

---
+++ pages/_app.js
@@ -0,0 +1,40 @@
+import { AuthProvider } from '@/lib/auth';
+import { CSSReset, ThemeProvider } from '@chakra-ui/core';
+import { Global, css } from '@emotion/core';
+import customTheme from '@/styles/theme';
+
+const GlobalStyle = ({ children }) => {
+  return (
+    <>
+      <CSSReset />
+      <Global
+        styles={css`
+          html {
+            min-width: 360px;
+            scroll-behavior: smooth;
+          }
+
+          #__next {
+            display: flex;
+            flex-direction: column;
+            min-height: 100vh;
+          }
+        `}
+      />
+      {children}
+    </>
+  );
+};
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <ThemeProvider theme={customTheme}>
+      <AuthProvider>
+        <GlobalStyle />
+        <Component {...pageProps} />
+      </AuthProvider>
+    </ThemeProvider>
+  );
+}
+
+export default MyApp;
